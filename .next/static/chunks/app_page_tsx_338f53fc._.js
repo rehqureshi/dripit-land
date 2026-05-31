8e1d3b14b84b86a273cf099187b461bc4e3bb409
rehqureshi <145358764+rehqureshi@users.noreply.github.com>
2026-06-01 01:39:58 +0530

    remove node_modules from tracking

--- .next/static/chunks/app_page_tsx_338f53fc._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_next_dist_aaa8db25._.js",
-  "static/chunks/_d2acfc6b._.js"
-],
-    source: "dynamic"
-});
